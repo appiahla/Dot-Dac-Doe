e84a00ef9cda2e667bb56d865c1da1871aea3b60
Khongorzul Munkhgerel <kmunkhg@gmail.com>
2022-02-25 11:08:14 -0500

    drop down menu code

--- app/src/main/java/com/example/dot_dac_doe/MainActivity.java
+++ app/src/main/java/com/example/dot_dac_doe/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.dot_dac_doe;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Spinner;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 

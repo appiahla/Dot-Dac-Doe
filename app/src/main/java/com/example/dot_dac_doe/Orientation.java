51d28c2d20f150eb9b1a238c11f19b4c493ef02f
Lois <appiahl2@vcu.edu>
2022-02-28 15:12:53 -0500

    added back btn from orientation to user-select

--- app/src/main/java/com/example/dot_dac_doe/Orientation.java
+++ app/src/main/java/com/example/dot_dac_doe/Orientation.java
@@ -2,7 +2,10 @@ package com.example.dot_dac_doe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Orientation extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Orientation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orientation);
+
+        ImageView back = findViewById(R.id.orientation_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Orientation.this, UserSelectActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

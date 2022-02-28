2f08c08d1dcd377c6090088baa036edbbaa88f10
Lois <appiahl2@vcu.edu>
2022-02-28 15:23:02 -0500

    added link from user-select to icon-select

--- app/src/main/java/com/example/dot_dac_doe/UserSelectActivity.java
+++ app/src/main/java/com/example/dot_dac_doe/UserSelectActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class UserSelectActivity extends AppCompatActivity {
@@ -19,6 +20,33 @@ public class UserSelectActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(UserSelectActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button orientation = findViewById(R.id.orientation_button_user_select);
+        orientation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserSelectActivity.this, Orientation.class);
+                startActivity(i);
+            }
+        });
+
+        ImageView player1 = findViewById(R.id.user_select_player1_circle);
+        player1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserSelectActivity.this, iconPage.class);
+                startActivity(i);
+            }
+        });
+
+        ImageView player2 = findViewById(R.id.user_select_player2_circle);
+        player2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserSelectActivity.this, iconPage.class);
                 startActivity(i);
             }
         });

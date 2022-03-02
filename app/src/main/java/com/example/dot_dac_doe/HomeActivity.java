6222e49ca9e93fc0b68f07c2204334edf24cd1ae
Cheyenne Kent <cheyennekent@Cheyennes-MacBook-Pro-233.local>
2022-03-01 20:24:58 -0500

    work in progress for connecting the home page to help page

--- app/src/main/java/com/example/dot_dac_doe/HomeActivity.java
+++ app/src/main/java/com/example/dot_dac_doe/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.dot_dac_doe;
+//import andoir
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        ImageView help = (ImageView) findViewByID(R.id.imageView6);
+//        help.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+//                startActivity(i);
+//            }
+//        });
+
     }
+
 }

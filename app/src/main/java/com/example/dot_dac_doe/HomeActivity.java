a5a7ea48958163eb47caa601b192edfc99997511
Cheyenne Kent <cheyennekent@Cheyennes-MacBook-Pro-233.local>
2022-03-01 21:31:46 -0500

    single player button activity to user select activity

--- app/src/main/java/com/example/dot_dac_doe/HomeActivity.java
+++ app/src/main/java/com/example/dot_dac_doe/HomeActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,6 +15,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn = (Button)findViewById(R.id.singlePlayer);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, UserSelectActivity.class));
+            }
+        });
+
+//        Button btn2 = (Button)findViewById(R.id.singlePlayer);
+//
+//        btn2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(HomeActivity.this, UserSelectActivity.class));
+//            }
+//        });
 
 //        ImageView help = (ImageView) findViewByID(R.id.imageView6);
 //        help.setOnClickListener(new View.OnClickListener() {

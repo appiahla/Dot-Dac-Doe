417cd222491434b5ca6abd7068805b827dc2ffea
Lois <appiahl2@vcu.edu>
2022-03-14 11:08:02 -0400

    added timeout for splash page to main page

--- app/src/main/java/com/example/dot_dac_doe/MainActivity.java
+++ app/src/main/java/com/example/dot_dac_doe/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.dot_dac_doe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static int TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }

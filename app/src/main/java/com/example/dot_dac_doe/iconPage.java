e9ba64eb7e6018fa46eaf225bae4902fbe932d56
Lois <appiahl2@vcu.edu>
2022-03-14 11:54:50 -0400

    added back functionality to icon page

--- app/src/main/java/com/example/dot_dac_doe/iconPage.java
+++ app/src/main/java/com/example/dot_dac_doe/iconPage.java
@@ -2,7 +2,11 @@ package com.example.dot_dac_doe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class iconPage extends AppCompatActivity {
@@ -11,5 +15,16 @@ public class iconPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icon_page);
+
+        ImageView orientation = findViewById(R.id.imageView5);
+        orientation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(iconPage.this, UserSelectActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }

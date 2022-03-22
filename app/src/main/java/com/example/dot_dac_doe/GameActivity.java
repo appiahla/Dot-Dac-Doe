9317e01321f32776754b3f886843025d03c32b17
Lois <appiahl2@vcu.edu>
2022-03-21 20:11:33 -0400

    added code to game activity

--- app/src/main/java/com/example/dot_dac_doe/GameActivity.java
+++ app/src/main/java/com/example/dot_dac_doe/GameActivity.java
@@ -2,7 +2,12 @@ package com.example.dot_dac_doe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +15,61 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public static class GameGrid extends View {
+
+        Paint black = new Paint();
+
+        public GameGrid(Context context) {
+            super(context);
+
+            black.setColor(Color.BLACK);
+            black.setStrokeWidth(8);
+        }
+
+        @Override
+        public void onDraw(Canvas canvas) {
+
+            float startX;
+            float stopX;
+            float startY;
+            float stopY;
+
+            int width = getWidth();
+            int height = getHeight();
+
+            int gridSize = 7;
+            int gridSpacing = Math.min(width, height) / gridSize;
+            int boardSize = gridSize * gridSpacing;
+
+            int xOffset = (width - boardSize)/2;
+            int yOffset = (height - boardSize)/2;
+
+            //Vertical Grid-lines
+            for (int i = 0; i < gridSize; i++) {
+
+                startX = xOffset + i*gridSpacing;
+                startY = yOffset;
+
+                stopX = startX;
+                stopY = startY + boardSize;
+
+                canvas.drawLine(startX, startY, stopX, stopY, black);
+
+            }
+
+            //Horizontal Grid-lines
+            for (int i = 0; i < gridSize; i++) {
+
+                startX = xOffset;
+                startY = yOffset + i*gridSpacing;
+
+                stopX = startX + boardSize;
+                stopY = startY;
+
+                canvas.drawLine(startX, startY, stopX, stopY, black);
+            }
+        }
     }
 }

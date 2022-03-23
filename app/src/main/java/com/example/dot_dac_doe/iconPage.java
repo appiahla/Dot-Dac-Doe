a446222ca6579146c0977004b90022b6718e2b96
kentca2 <kentca2@vcu.edu>
2022-03-22 22:01:37 -0400

    added clickable functionality for peach that puts it in the icon circle

--- app/src/main/java/com/example/dot_dac_doe/iconPage.java
+++ app/src/main/java/com/example/dot_dac_doe/iconPage.java
@@ -72,6 +72,14 @@ public class iconPage extends AppCompatActivity {
             }
         });
 
+        ImageView PeachClick = findViewById(R.id.peach);
+        PeachClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Icon1.setImageResource(R.drawable.peach);
+            }
+        });
+
     }
 
 

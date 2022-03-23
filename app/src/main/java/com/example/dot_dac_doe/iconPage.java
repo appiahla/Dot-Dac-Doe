0555801d5f15382b5c25fddd018a725eeb0bd076
kentca2 <kentca2@vcu.edu>
2022-03-22 22:03:16 -0400

    added clickable functionality for Rain that puts it in the icon circle

--- app/src/main/java/com/example/dot_dac_doe/iconPage.java
+++ app/src/main/java/com/example/dot_dac_doe/iconPage.java
@@ -88,6 +88,14 @@ public class iconPage extends AppCompatActivity {
             }
         });
 
+        ImageView RainClick = findViewById(R.id.rain);
+        RainClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Icon1.setImageResource(R.drawable.rain);
+            }
+        });
+
     }
 
 

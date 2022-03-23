47c681e174510fb5db0c21edfeb0d2470d906518
kendalljb20 <90643648+kendalljb20@users.noreply.github.com>
2022-03-23 12:13:18 -0400

    adding functionality to play_again button

--- app/src/main/java/com/example/dot_dac_doe/winner_page.java
+++ app/src/main/java/com/example/dot_dac_doe/winner_page.java
@@ -2,7 +2,11 @@ package com.example.dot_dac_doe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class winner_page extends AppCompatActivity {
@@ -11,5 +15,13 @@ public class winner_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner_page);
+        Button play_again = findViewById(R.id.play_again);
+        play_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(winner_page.this, UserSelectActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

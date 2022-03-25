3a075c9849bcd23facca28147cabe637e97f9b10
kendalljb20 <90643648+kendalljb20@users.noreply.github.com>
2022-03-25 12:18:20 -0400

    code to display winner page

--- app/src/main/java/com/example/dot_dac_doe/winner_page.java
+++ app/src/main/java/com/example/dot_dac_doe/winner_page.java
@@ -1,12 +1,17 @@
 package com.example.dot_dac_doe;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.example.dot_dac_doe.model.Player;
+
 
 
 public class winner_page extends AppCompatActivity {
@@ -17,6 +22,7 @@ public class winner_page extends AppCompatActivity {
         setContentView(R.layout.activity_winner_page);
         Button play_again = findViewById(R.id.play_again);
         Button home_button = findViewById(R.id.home_button);
+        setContentView(R.layout.activity_multiplayer_game);
         play_again.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,13 +30,37 @@ public class winner_page extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        home_button.setOnClickListener(new View.OnClickListener(){
+        home_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent a = new Intent(winner_page.this, HomeActivity.class);
                 startActivity(a);
             }
         });
+    }
+            public void setWinner(final Player winner) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        new AlertDialog.Builder(winner_page.this)
+                                .setTitle("Dots And Boxes")
+                                .setMessage(winner.getName() + " Wins!")
+                                .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        recreate();
+                                    }
+                                })
+                                .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                    }
+                                }).show();
+                    }
+                });
             }
 
-    }
+
+
+
+}

8cd0ec21211bd1b7701f320675dd67c893a6a5c3
Lois <appiahl2@vcu.edu>
2022-03-25 12:12:34 -0400

    created horizontal pause menu files

---
+++ app/src/main/java/com/example/dot_dac_doe/Hori_exit_confirmation.java
@@ -0,0 +1,14 @@
+package com.example.dot_dac_doe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hori_exit_confirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hori_exit_confirmation);
+    }
+}

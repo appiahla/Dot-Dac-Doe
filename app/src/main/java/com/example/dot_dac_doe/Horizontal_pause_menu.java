2b30998d4e7c9895e728980ca2d8da6c8a3bfa5e
Lois <appiahl2@vcu.edu>
2022-03-26 11:44:25 -0400

    added references for new pause menu pages

--- app/src/main/java/com/example/dot_dac_doe/Horizontal_pause_menu.java
+++ app/src/main/java/com/example/dot_dac_doe/Horizontal_pause_menu.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class exit_confirmation extends AppCompatActivity {
+public class Horizontal_pause_menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vertical_exit_confirmation);
+        setContentView(R.layout.hori_pause_menu);
     }
 }

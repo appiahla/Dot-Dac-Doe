0ed9221d97fb2026e3a50e751e79201280128024
kendalljb20 <90643648+kendalljb20@users.noreply.github.com>
2022-03-28 12:43:43 -0400

    working on how to display who won

--- app/src/main/java/com/example/dot_dac_doe/winner_page.java
+++ app/src/main/java/com/example/dot_dac_doe/winner_page.java
@@ -25,25 +25,22 @@ public class winner_page extends AppCompatActivity {
         setContentView(R.layout.activity_winner_page);
         Button play_again = findViewById(R.id.play_again);
         Button home_button = findViewById(R.id.home_button);
-        setContentView(R.layout.activity_multiplayer_game);
+//        setContentView(R.layout.activity_multiplayer_game);
         TextView winnerLabel = (TextView) findViewById(R.id.winner_header);
         TextView congrats = (TextView) findViewById(R.id.congrats);
-        String winner = getIntent().getStringExtra("WINNER");
-        //int score = getIntent().getIntExtra("SCORE", 0);
-        int player1Score = getIntent().getIntExtra("WINNER", 0);
-        int player2Score = getIntent().getIntExtra("WINNER", 0);
-        SharedPreferences gameSettings = getSharedPreferences("DATA", Context.MODE_PRIVATE);
-
-        if (player1Score > player2Score){
-            winnerLabel.setText("CONGRATULATIONS PLAYER 1! "+ player1Score);
-
-        }else{
-            winnerLabel.setText("CONGRATULATIONS PLAYER 2! " + player2Score);
-        }
-
-
-
-
+//        String winner = getIntent().getStringExtra("WINNER");
+//        //int score = getIntent().getIntExtra("SCORE", 0);
+//        int player1Score = getIntent().getIntExtra("WINNER", 0);
+//        int player2Score = getIntent().getIntExtra("WINNER", 0);
+//        SharedPreferences gameSettings = getSharedPreferences("DATA", Context.MODE_PRIVATE);
+//
+//        if (player1Score > player2Score){
+//            winnerLabel.setText("CONGRATULATIONS PLAYER 1! "+ player1Score);
+//
+//        }else{
+//            winnerLabel.setText("CONGRATULATIONS PLAYER 2! " + player2Score);
+//
+//        }
 
 
         play_again.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +49,7 @@ public class winner_page extends AppCompatActivity {
                 Intent i = new Intent(winner_page.this, UserSelectActivity.class);
                 startActivity(i);
                 Intent intent = new Intent(getApplicationContext(),winner_page.class);
-                intent.putExtra("WINNER", winner);
+//                intent.putExtra("WINNER", winner);
                 startActivity(intent);
             }
         });
@@ -63,39 +60,39 @@ public class winner_page extends AppCompatActivity {
                 startActivity(a);
             }
         });
-        Button restart = (Button) findViewById(R.id.vertical_restart);
-        restart.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-                Intent i = new Intent(winner_page.this, MultiplayerGame.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(i);
-            }
-        });
-
-
+//      Button restart = (Button) findViewById(R.id.vertical_restart);
+//        restart.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent i = new Intent(winner_page.this, MultiplayerGame.class);
+//                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(i);
+//            }
+//        });
+//
+//
         }
-            public void setWinner(final Player winner) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        new AlertDialog.Builder(winner_page.this)
-                                .setTitle("Dots And Boxes")
-                                .setMessage(winner.getName() + " Wins!")
-                                .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        recreate();
-                                    }
-                                })
-                                .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                    }
-                                }).show();
-                    }
-                });
-            }
+//            public void setWinner(final Player winner) {
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        new AlertDialog.Builder(winner_page.this)
+//                                .setTitle("Dots And Boxes")
+//                                .setMessage(winner.getName() + " Wins!")
+//                                .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int which) {
+//                                        recreate();
+//                                    }
+//                                })
+//                                .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+//                                    @Override
+//                                    public void onClick(DialogInterface dialogInterface, int i) {
+//                                    }
+//                                }).show();
+//                    }
+//                });
+//            }
 
 }

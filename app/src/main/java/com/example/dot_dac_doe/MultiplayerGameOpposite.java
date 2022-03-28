da4b1bd9c8da1312fa17be0480e926c87b47ad97
Khongorzul Munkhgerel <kmunkhg@gmail.com>
2022-03-28 01:27:59 -0400

    adjusting playernow pointer in multiplayer java files

--- app/src/main/java/com/example/dot_dac_doe/MultiplayerGameOpposite.java
+++ app/src/main/java/com/example/dot_dac_doe/MultiplayerGameOpposite.java
@@ -39,7 +39,7 @@ public class MultiplayerGameOpposite extends AppCompatActivity implements Player
         player2name = (TextView) findViewById(R.id.player2name_mult3);
         player1points = (TextView) findViewById(R.id.player1points_mult3);
         player2points = (TextView) findViewById(R.id.player2points_mult3);
-        currentPlayerPointer = (ImageView) findViewById(R.id.playerNowPointer3);
+        currentPlayerPointer = (ImageView) findViewById(R.id.playerNowPointer_mult3);
 
         players = new Player[]{new HumanPlayer("Human"), new RandomAIPlayer("Computer")};
         startGame(players);

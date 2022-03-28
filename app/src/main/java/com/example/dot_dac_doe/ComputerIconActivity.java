42ddba0c9a32d0956f72a2e04d249f6c9d362f2a
Lois <appiahl2@vcu.edu>
2022-03-28 18:34:26 -0400

    created blank computer icon select activity

---
+++ app/src/main/java/com/example/dot_dac_doe/ComputerIconActivity.java
@@ -0,0 +1,14 @@
+package com.example.dot_dac_doe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComputerIconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computer_icon);
+    }
+}

46318f1d083f025b1abdefa5a8615116e1644055
Khongorzul Munkhgerel <kmunkhg@gmail.com>
2022-03-28 00:51:18 -0400

    adding new files to manifest file

--- app/src/main/java/com/example/dot_dac_doe/MultiplayerGameOppositeVertical.java
+++ app/src/main/java/com/example/dot_dac_doe/MultiplayerGameOppositeVertical.java
@@ -18,7 +18,7 @@ import com.example.dot_dac_doe.view.PlayersStateView;
 
 import java.util.Map;
 
-public class MultiplayerGameSame extends AppCompatActivity implements PlayersStateView {
+public class MultiplayerGameOppositeVertical extends AppCompatActivity implements PlayersStateView {
 
     protected GameView gameView;
     protected TextView player1name, player2name, player1points, player2points;
@@ -30,17 +30,16 @@ public class MultiplayerGameSame extends AppCompatActivity implements PlayersSta
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mulitplayer_game_same;
-        //setContentView(R.layout.activity_game_opposite);
+        setContentView(R.layout.activity_multiplayer_game_opposite_vertical);
 
-        gameView = (GameView) findViewById(R.id.gameView_mult2);
+        gameView = (GameView) findViewById(R.id.gameView_mult4);
         gameView.setPlayersState(this);
 
-        player1name = (TextView) findViewById(R.id.player1name_mult2);
-        player2name = (TextView) findViewById(R.id.player2name_mult2);
-        player1points = (TextView) findViewById(R.id.player1points_mult2);
-        player2points = (TextView) findViewById(R.id.player2points_mult2);
-        currentPlayerPointer = (ImageView) findViewById(R.id.playerNowPointer2);
+        player1name = (TextView) findViewById(R.id.player1name_mult4);
+        player2name = (TextView) findViewById(R.id.player2name_mult4);
+        player1points = (TextView) findViewById(R.id.player1points_mult4);
+        player2points = (TextView) findViewById(R.id.player2points_mult4);
+        currentPlayerPointer = (ImageView) findViewById(R.id.playerNowPointer4);
 
         players = new Player[]{new HumanPlayer("Human"), new RandomAIPlayer("Computer")};
         startGame(players);
@@ -84,7 +83,7 @@ public class MultiplayerGameSame extends AppCompatActivity implements PlayersSta
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                new AlertDialog.Builder(MultiplayerGameSame.this)
+                new AlertDialog.Builder(MultiplayerGameOppositeVertical.this)
                         .setTitle("Dots And Boxes")
                         .setMessage(winner.getName() + " Wins!")
                         .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
@@ -115,12 +114,12 @@ public class MultiplayerGameSame extends AppCompatActivity implements PlayersSta
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    new AlertDialog.Builder(MultiplayerGameSame.this)
+                    new AlertDialog.Builder(MultiplayerGameOppositeVertical.this)
                             .setTitle("Dots And Boxes")
                             .setMessage("New game versus")
                             .setPositiveButton("Computer", new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
-                                    new AlertDialog.Builder(MultiplayerGameSame.this)
+                                    new AlertDialog.Builder(MultiplayerGameOppositeVertical.this)
                                             .setTitle("Who goes first?")
                                             .setPositiveButton("Computer", new DialogInterface.OnClickListener() {
                                                 @Override
